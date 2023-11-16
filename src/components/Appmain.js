732977abea8e94e5d862500d6cfda8cf637dad32
Saikalyan Labhishetty <89219380+SaikalyanLabhishetty@users.noreply.github.com>
2023-11-16 13:12:01 +0530

    Update Appmain.js

--- src/components/Appmain.js
+++ src/components/Appmain.js
@@ -1,95 +1,51 @@
 import React from 'react';
-import styled from 'styled-components';
 import Appheader from './Appheader';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
-import Carousel from '@itseasy21/react-elastic-carousel';
-import { locations, breakpoints, property, breakpoints_2 } from './export';
+import { locations, property} from './export';
 import Appfooter from './Appfooter';
-
-const Section1 = styled.div`
-  height: 40.25rem;
-  background-image: url(https://i.imgur.com/vKX2T7P.gif);
-  background-size: 100% 100%;
-  color: white;
-  font-family: 'Unbounded', sans-serif;
-`;
-
-const TextContainer = styled.p`
-  padding-top: 18rem;
-  padding-left: 3rem;
-  width: 30rem;
-  font-size: 2rem;
-  background-color: transparent;
-`;
-
-const HighlightedText = styled.span`
-  color: #c0c0c0;
-  font-size: 2rem;
-  background: transparent;
-`;
-
-const SliderTitle = styled.h1`
-  color: white;
-  text-align: center;
-  margin-top: 3rem;
-`;
-
-const CarouselContainer = styled(Carousel)`
-  color: white;
-  margin-top: 2rem;
-  margin-bottom: 3rem;
-`;
-
-const PlaceImage = styled.img`
-  height: 10rem;
-  width: 15rem;
-  border-radius: 1rem;
-`;
-
-const SliderContainer = styled.div`
-  text-align: center;
-`;
+import Marquee from 'react-fast-marquee';
+import "./Appmain.css"
 
 const AppMain = () => {
   const [text] = useTypewriter({
     words: ['Hotels', 'Apartments', 'Resorts', 'Villas', 'Cabins', 'Cottages', 'Glamping', 'Serviced Apartments', 'Vacation Homes', 'Guest House', 'Hostel', 'Motels'],
     loop: 0,
   });
-
   return (
     <>
-      <Appheader />
-      <Section1>
-        <TextContainer>
-          Booker.io helps you to book <HighlightedText>{text}</HighlightedText>
-          <Cursor cursorColor='red' />
-        </TextContainer>
-      </Section1>
-      <SliderContainer>
-        <SliderTitle>Book anywhere in India</SliderTitle>
-        <CarouselContainer breakPoints={breakpoints} className='carousel'>
-          {locations.map((locat) => (
-            <div key={locat.id}>
-              <PlaceImage src={locat.image} alt={locat.title} />
-              <p style={{ textAlign: 'center' }}>{locat.title}</p>
-            </div>
-          ))}
-        </CarouselContainer>
-      </SliderContainer>
-      <SliderContainer>
-        <SliderTitle>List of properties available</SliderTitle>
-        <CarouselContainer breakPoints={breakpoints_2} className='carousel'>
-          {property.map((prop) => (
-            <div key={prop.id}>
-              <PlaceImage src={prop.image} alt={prop.name} />
-              <p style={{ textAlign: 'center' }}>{prop.name}</p>
-            </div>
-          ))}
-        </CarouselContainer>
-      </SliderContainer>
-      <Appfooter />
+    <Appheader/>
+    <div className='section-1'>
+      <p>
+      Booker.io helps you to book <span>{text}</span>
+      <Cursor cursorColor='red' />
+      </p>
+    </div>
+    <div className='slider'>
+      <h1> Select Your location</h1>
+    <Marquee direction="left" speed={80} style={{marginBottom:"3rem"}}>
+              {locations.map((locat, index) => (
+                <div key={index} className="wrapper-class-1">
+                  <img src={locat.image} alt={locat.name}/>
+                  <p style={{ color: 'white', textAlign:"center" }}>{locat.title}</p>
+                </div>
+              ))}
+    </Marquee>
+    </div>
+    <div className='slider-2'>
+    <h1> List of properties</h1>
+    <Marquee direction="left" speed={80} style={{marginBottom:"3rem"}}>
+              {property.map((prop, index) => (
+                <div key={index} className="wrapper-class">
+                  <img src={prop.image} alt={prop.name}/>
+                  <p style={{ color: 'white', textAlign:"center" }}>{prop.name}</p>
+                </div>
+              ))}
+    </Marquee>
+    </div>
+    <Appfooter/>
     </>
-  );
-};
+  )
+}
+
 
 export default AppMain;
